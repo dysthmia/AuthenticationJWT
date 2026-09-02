403bda657f1b5a6cd2be1a4d5835a612778b8a18
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-09-02 18:07:49 +0300

    add context & configurations

---
+++ src/Infrastructure/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public sealed class AuthDbContext (DbContextOptions<DbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AuthDbContext).Assembly);
+    }
+}
---
+++ src/Infrastructure/UserConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+
+        // Primary Key
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(
+                userId => userId.Value,
+                value => UserId.From(value))
+            .HasColumnName("id")
+            .HasMaxLength(17)
+            .IsRequired();
+
+        // PersonalData как Value Object (struct) -> ComplexProperty, а не OwnsOne
+        builder.ComplexProperty(
+            x => x.PersonalData,
+            personalData =>
+            {
+                personalData.Property(x => x.FirstName)
+                    .HasColumnName("first_name")
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                personalData.Property(x => x.LastName)
+                    .HasColumnName("last_name")
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                personalData.Property(x => x.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(254)
+                    .IsRequired();
+            });
+
+        builder.Property(x => x.RegisteredAt)
+            .HasColumnName("registered_at")
+            .IsRequired();
+
+        builder.Property(x => x.IsActive)
+            .HasColumnName("is_active")
+            .IsRequired();
+    }
+}
