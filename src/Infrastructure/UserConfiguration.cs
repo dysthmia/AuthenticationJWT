0437b1de2854077411244c07d7455481036485e8
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-09-03 23:13:35 +0300

    update user configurations, add login information like value object

--- src/Infrastructure/UserConfiguration.cs
+++ src/Infrastructure/UserConfiguration.cs
@@ -42,6 +42,29 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
                     .IsRequired();
             });
 
+        builder.Property(x => x.PasswordHash)
+            .HasColumnName("password_hash")
+            .HasMaxLength(200) 
+            .IsRequired();
+
+        // LoginInformation как Value Object
+        builder.ComplexProperty(
+            x => x.LoginInformation,
+            loginInformation =>
+            {
+                loginInformation.Property(x => x.FailedLoginAttempts)
+                    .HasColumnName("failed_login_attempts")
+                    .IsRequired();
+
+                loginInformation.Property(x => x.LockoutEnd)
+                    .HasColumnName("lockout_end")
+                    .IsRequired(false);
+
+                loginInformation.Property(x => x.LastLoginAt)
+                    .HasColumnName("last_login_at")
+                    .IsRequired(false);
+            });
+
         builder.Property(x => x.RegisteredAt)
             .HasColumnName("registered_at")
             .IsRequired();
